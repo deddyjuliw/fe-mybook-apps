69b2439984626ad9f706eb3aea9369f5edea48ed
Deddy Juli W As <96725715+deddyjuliw@users.noreply.github.com>
2022-10-28 23:49:00 +0700

    Add files via upload

---
+++ bookshelf-apps/js/script.js
@@ -0,0 +1,270 @@
+const input_book = document.getElementById('inputBook');
+const search_books = document.getElementById('searchBook');
+const title_bar = document.getElementById('title_form');
+
+document.addEventListener('DOMContentLoaded', function() {
+    input_book.addEventListener('submit', (event) => {
+        event.preventDefault();
+        add_book();
+    })
+
+    search_books.addEventListener('submit', (event) => {
+        event.preventDefault();
+        search_book();
+    })
+
+    if (is_storage_exist()) {
+        load_data_from_storage();
+    }
+})
+
+const RENDER_BOOK = 'render-book';
+const SAVE_EVENT = 'save-books';
+const STORAGE_KEY = 'BOOKSELF_APP';
+const to_book = [];
+const found__book = [];
+
+function generate_id() {
+    return +new Date();
+}
+
+function generate_book(id, title, author, year, isComplete) {
+    return {
+        id,
+        title,
+        author,
+        year,
+        isComplete
+    }
+}
+
+function find_book(book_id) {
+    for (const book of to_book) {
+        if (book.id === book_id) {
+            return book;
+        }
+    }
+    return null;
+}
+
+document.addEventListener(RENDER_BOOK, function() {
+    const complete_book = document.getElementById('completeBookshelfList');
+    const incomplete_book = document.getElementById('incompleteBookshelfList');
+    const key_book = document.getElementById('searchBookTitle').value;
+    complete_book.innerHTML = '';
+    incomplete_book.innerHTML = '';
+
+    if (key_book == '') {
+        for (const book of to_book) {
+            const card_book = make_card_book(book);
+            if (book.isComplete) {
+                complete_book.append(card_book);
+            } else {
+                incomplete_book.append(card_book);
+            }
+        }
+    } else {
+        for (const book of found__book) {
+            const card_book = make_card_book(book);
+            if (book.isComplete) {
+                complete_book.append(card_book);
+            } else {
+                incomplete_book.append(card_book);
+            }
+        }
+    }
+})
+
+
+function handler_complete_to_read(book_id) {
+    const book_target = find_book(book_id);
+
+    if (book_target === null) return;
+
+    book_target.isComplete = false;
+    document.dispatchEvent(new Event(RENDER_BOOK));
+    save_data();
+}
+
+function handler_incomplete_to_read(book_id) {
+    const book_target = find_book(book_id);
+
+    if (book_target === null) return;
+
+    book_target.isComplete = true;
+    document.dispatchEvent(new Event(RENDER_BOOK));
+    save_data();
+}
+
+function handler_remove_book(book_id) {
+    const book_target = find_to_index(book_id);
+
+    if (book_target === -1) return;
+
+    to_book.splice(book_target, 1);
+    document.dispatchEvent(new Event(RENDER_BOOK));
+    save_data();
+}
+
+function find_to_index(book_id) {
+    for (const index in to_book) {
+        if (to_book[index].id === book_id) {
+            return index;
+        }
+    }
+
+    return -1;
+}
+
+function make_card_book({ id, title, author, year, isComplete }) {
+
+    const title_element = document.createElement('h4');
+    title_element.innerText = `${title}`;
+
+    const author_element = document.createElement('p');
+    author_element.innerText = `Writer : ${author}`;
+
+    const year_element = document.createElement('p');
+    year_element.innerText = `Year : ${year}`;
+
+    const btn_blue = document.createElement('button');
+    btn_blue.classList.add('blue');
+
+    const btn_red = document.createElement('button');
+    btn_red.classList.add('red');
+    btn_red.innerHTML = `<i class="uil uil-trash-alt"></i>`;
+
+    btn_red.addEventListener('click', function() {
+        const confirm_remove = confirm("Are you sure want to delete it?");
+        if (confirm_remove === true) return handler_remove_book(id);
+    })
+
+    const div_element = document.createElement('div');
+    div_element.classList.add('action');
+    div_element.append(btn_blue, btn_red);
+
+    const article_element = document.createElement('article');
+    article_element.classList.add('book_item');
+    article_element.setAttribute('id', `${id}`);
+
+    article_element.append(title_element, author_element, year_element, div_element);
+
+    if (isComplete) {
+        btn_blue.innerHTML = `<i class="uil uil-book-alt"></i>`;
+
+        btn_blue.addEventListener('click', function() {
+            handler_complete_to_read(id);
+        })
+
+    } else {
+        btn_blue.innerHTML = `<i class="uil uil-book-open"></i>`;
+
+        btn_blue.addEventListener('click', function() {
+            handler_incomplete_to_read(id);
+        })
+    }
+
+    return article_element;
+}
+
+
+function input_form_data() {
+    const form_data = {};
+
+    form_data['title'] = document.getElementById('inputBookTitle').value;
+    form_data['author'] = document.getElementById('inputBookAuthor').value;
+    form_data['year'] = document.getElementById('inputBookYear').value;
+    form_data['isComplete'] = document.getElementById('inputBookIsComplete').checked;
+
+    return form_data;
+}
+
+function reset_form_data() {
+    document.getElementById('inputBookTitle').value = '';
+    document.getElementById('inputBookAuthor').value = '';
+    document.getElementById('inputBookYear').value = '';
+    document.getElementById('inputBookIsComplete').checked = false;
+    return;
+}
+
+
+
+// Handler add book
+function add_book() {
+
+    reset_form_search()
+    const { title, author, year, isComplete } = input_form_data();
+    const id = generate_id();
+    const books = generate_book(id, title, author, year, isComplete);
+    reset_form_data();
+
+    if (to_book.push(books)) {
+        title_bar.innerText = "SAVED!";
+        title_bar.classList.add('success');
+    }
+
+    document.dispatchEvent(new Event(RENDER_BOOK));
+
+    save_data();
+}
+
+// Handler search book
+function search_book() {
+
+    const key_book = document.getElementById('searchBookTitle').value
+    const book = find_title_book(key_book);
+    if (book == -1 || found__book.length > 1) {
+        found__book.splice(0, found__book.length);
+    } else if (found__book.length == 1) {
+        return false;
+    } else {
+        found__book.push(book);
+    }
+
+    document.dispatchEvent(new Event(RENDER_BOOK));
+
+    save_data();
+}
+
+function find_title_book(title_book) {
+
+    for (const index in to_book) {
+        if (to_book[index].title == title_book) {
+            return to_book[index];
+        }
+    }
+    return -1
+}
+
+function reset_form_search() {
+    document.getElementById('searchBookTitle').value = '';
+    return;
+}
+
+function save_data() {
+    if (is_storage_exist()) {
+        const parsed = JSON.stringify(to_book);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVE_EVENT));
+    }
+}
+
+function is_storage_exist() {
+    if (typeof(Storage) === undefined) {
+        alert('Local Storage is not support in this Browser');
+        return false;
+    }
+    return true;
+}
+
+function load_data_from_storage() {
+    const serialize_data = localStorage.getItem(STORAGE_KEY);
+    let data_object = JSON.parse(serialize_data);
+
+    if (data_object !== null) {
+        for (const books of data_object) {
+            to_book.push(books);
+        }
+    }
+    document.dispatchEvent(new Event(RENDER_BOOK));
+}
